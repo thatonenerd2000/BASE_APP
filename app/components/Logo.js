9533fd792fe003b61dd101e2074affb225f8ebdb
Shadman Chowdhury <shadmanchowdhury@Shadmans-MBP.home>
2018-11-11 01:14:28 -0500

    splash

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -2,29 +2,63 @@ import React, { Component } from "react";
 import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
 
 import BaseLogo from "../../media/logo.png";
+import FadeAnimation from "./animations/fadeAnimation.js";
+import ScaleAnimation from "./animations/scaleAnimation.js";
+import TranslateAnimation from "./animations/translateAnimation.js";
 
 export default class Logo extends Component {
   render() {
     const vHeight = Dimensions.get("window").height;
     const vWidth = Dimensions.get("window").width;
+
+    const logPosition = {
+      top: vHeight - vHeight / 1.13,
+      height: vHeight / 3,
+      width: vWidth / 3
+    };
+
     return (
-      <View
-        style={{
-          height: vHeight / 2,
-          width: vWidth / 2,
-          top: vHeight / 20,
-          left: vWidth / 4
-        }}
-      >
-        <Image
-          source={BaseLogo}
-          style={{
-            resizeMode: "contain",
-            flex: 1,
-            height: "100%",
-            width: "100%"
-          }}
-        />
+      <View>
+        <FadeAnimation>
+          <ScaleAnimation>
+            <TranslateAnimation>
+              <View
+                style={{
+                  height: logPosition.height,
+                  width: logPosition.width,
+                  top: logPosition.top,
+                  left: vWidth / 3
+                }}
+              >
+                <Image
+                  source={BaseLogo}
+                  style={{
+                    resizeMode: "contain",
+                    flex: 1,
+                    height: "100%",
+                    width: "100%"
+                  }}
+                />
+              </View>
+            </TranslateAnimation>
+          </ScaleAnimation>
+        </FadeAnimation>
+        <FadeAnimation>
+          <View>
+            <Text
+              style={{
+                top: logPosition.top + logPosition.height / 6,
+                textAlign: "center",
+                color: "white",
+                fontFamily: "AppleSDGothicNeo-Bold",
+                fontSize: vHeight / 15,
+                color: "#65b3a9"
+              }}
+            >
+              B.A.S.E.
+            </Text>
+          </View>
+        </FadeAnimation>
       </View>
     );
   }
