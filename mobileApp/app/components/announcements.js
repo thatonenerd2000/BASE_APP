8f75084a73401457f2767ffec5df3cf9c70359ad
Shadman <chowdhuryshadman0@gmail.com>
2018-12-31 02:02:23 -0500

    posted x days ago function, tnx bright

--- mobileApp/app/components/announcements.js
+++ mobileApp/app/components/announcements.js
@@ -11,10 +11,44 @@ import * as firebase from "firebase";
 export default class Announcements extends Component {
   constructor(props) {
     super(props);
+    this.getTimeSinceDate = this.getTimeSinceDate.bind(this);
+    this.getTimeSincePost = this.getTimeSincePost.bind(this);
     this.state = {
       announcements: []
     };
   }
+  //Brights Algorithm
+  getTimeSinceDate = dateThen => {
+    var dateNow = Date.now();
+
+    var diff = dateNow - dateThen; // milliseconds
+
+    var time = {
+      years: Math.floor(diff / (1000 * 60 * 60 * 24 * 30 * 12)),
+      months: Math.floor(diff / (1000 * 60 * 60 * 24 * 30)) % 12,
+      days: Math.floor(diff / (1000 * 60 * 60 * 24)) % 30,
+      hours: Math.floor(diff / (1000 * 60 * 60)) % 24,
+      minutes: Math.floor(diff / (1000 * 60)) % 60,
+      seconds: Math.floor(diff / 1000) % 60
+    };
+
+    return time;
+  };
+
+  getTimeSincePost = dateThen => {
+    var time = this.getTimeSinceDate(dateThen);
+
+    if (time.years > 0) return <Text>Posted {time.years} years ago</Text>;
+    else if (time.months > 0)
+      return <Text>Posted {time.months} months ago</Text>;
+    else if (time.days > 0) return <Text>Posted {time.days} days ago</Text>;
+    else if (time.hours > 0) return <Text>Posted {time.hours} hours ago</Text>;
+    else if (time.minutes > 0)
+      return <Text>Posted {time.minutes} minutes ago</Text>;
+    else if (time.seconds > 0)
+      return <Text>Posted {time.seconds} seconds ago</Text>;
+    else return "Posted just now";
+  };
 
   componentDidMount() {
     if (!firebase.apps.length) {
@@ -45,7 +79,6 @@ export default class Announcements extends Component {
   }
 
   render() {
-    //let current = new Date();
     let ScreenHeight = Dimensions.get("window").height;
     let announcement = this.state.announcements.map(sub => {
       return (
@@ -56,24 +89,23 @@ export default class Announcements extends Component {
               borderStyle: "solid",
               borderWidth: 2,
               borderRadius: 20,
-              padding: 20,
+              padding: 25,
               backgroundColor: "#0e141d",
-              height: null
+              height: null,
+              textAlign: "left"
             }}
           >
             <Text
               style={{
                 fontWeight: "bold",
                 fontSize: 18,
-                textAlign: "center",
+                textAlign: "left",
                 color: "white"
               }}
             >
               {sub.Subject}
             </Text>
-            <Text
-              style={{ textAlign: "center", color: "#42bff4", fontSize: 10 }}
-            >
+            <Text style={{ textAlign: "left", color: "#42bff4", fontSize: 10 }}>
               {sub.PostedDate}
             </Text>
             <View
@@ -84,9 +116,24 @@ export default class Announcements extends Component {
               }}
             />
             <Text
-              style={{ textAlign: "center", color: "white", marginTop: 10 }}
+              style={{
+                textAlign: "left",
+                color: "white",
+                marginTop: 10
+              }}
             >
               {sub.Content}
+            </Text>
+            <Text
+              style={{
+                textAlign: "left",
+                color: "white",
+                fontSize: 10,
+                marginTop: 5,
+                color: "grey"
+              }}
+            >
+              {this.getTimeSincePost(sub.CompareDate)}
             </Text>
           </View>
         </TouchableOpacity>
